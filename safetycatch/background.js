9762acf7e45ce7ccff1dbe43eb10a9627c4f1e0b
henryoz <henry@osadzinski.co.uk>
2014-12-15 09:14:08 +0000

    oops

--- safetycatch/background.js
+++
@@ -1,2 +0,0 @@
-alert("Safety is ON. Click the icon to switch off.");
-$('input[value="Reset to defaults"]').attr('disabled','disabled').css('opacity','0.1');
--- safetycatch/trigger.js
+++
@@ -1,2 +0,0 @@
-alert("Safety is OFF. Be careful! Reload the page to switch back on.");
-$('input[value="Reset to defaults"]').removeAttr('disabled').css('opacity','1'); 
