14a2e46535ef7aaab86580adfd1a5b4e75000a30
henryoz <henry@osadzinski.co.uk>
2014-12-15 09:10:01 +0000

    copypasta commit

---
+++ safetycatch/background.js
@@ -0,0 +1,2 @@
+alert("Safety is ON. Click the icon to switch off.");
+$('input[value="Reset to defaults"]').attr('disabled','disabled').css('opacity','0.1');
---
+++ safetycatch/trigger.js
@@ -0,0 +1,2 @@
+alert("Safety is OFF. Be careful! Reload the page to switch back on.");
+$('input[value="Reset to defaults"]').removeAttr('disabled').css('opacity','1'); 
