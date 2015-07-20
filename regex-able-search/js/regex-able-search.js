5c8de8738945052391c77584087ef13caadd0318
Kimbsy <dave.kimber@simitive.com>
2015-07-20 15:18:00 +0100

    #regex-able-search wrapping/unwrapping sorted, just need the wrapping to replace the content with the match, not the search string

--- regex-able-search/js/regex-able-search.js
+++ regex-able-search/js/regex-able-search.js
@@ -19,6 +19,10 @@ $(document).ready(function() {
       if (showing) {
         searchContainer.slideUp();
         showing = false;
+
+        // remove_html_tag('span', 'html');
+        // $('html').regexReplace('<\/?span[^>]*>');
+        $('span.regex-match').contents().unwrap();
       }
       else {
         searchContainer.slideDown();
@@ -29,16 +33,89 @@ $(document).ready(function() {
       }
     }
 
-    // submit serach string
+    // submit search string
     if (showing) {
       if (e.keyCode == 13) {
-        regexSearch(searchInput.val());
+        $('html').regexReplace(searchInput.val());
       }
     }
   });
 
 });
 
-function regexSearch(regexString) {
-  console.log(regexString);
+$.fn.regexReplace = function(regexString) {
+
+  var pattern = new RegExp(regexString, 'g');
+  var repl = '<span class="regex-match">' + regexString + '</span>';
+   
+  this.each(function() {
+    $(this).contents().each(function() {
+      if(this.nodeType === 3 && pattern.test(this.nodeValue)) {
+        $(this).replaceWith(this.nodeValue.replace(pattern, repl));
+      }
+      else if(!$(this).hasClass('high')) {
+        $(this).regexReplace(regexString);
+      }
+    });
+  });
+  return this;
+
+  // // create regex string
+  // var regex = new RegExp('(' + regexString + ')', 'g');
+
+  // // console.log('wooooo');
+
+  // // // get text from page
+  // // // var pageHTML = $('html').html();
+
+  // // get all nodes
+  // var nodes = $('*');
+
+  // nodes.each(function() {
+  //   // console.log($(this).text());
+  //   if ($(this).text()) {
+  //     console.log($(this).text());
+  //     var oldText = $(this).text() || '';
+  //     $(this).text(oldText.replace(regex, '<span class="regex-match">$1</span>'));
+  //   }
+  // });
+};
+
+function removeSpans() {
+  var spans = document.getElementsByTagName("span");
+
+  for(var i=0; i<spans.length;i++)
+  {
+    if(spans[i].className == "regex-match")
+    {
+      var container = spans[i].parentNode;
+      var text = spans[i].innerHTML;
+      container.innerHTML += text;
+      container.removeChild(spans[i]);
+    }
+    else if(spans[i].className == "removed")
+    {
+      var container = spans[i].parentNode;
+      container.removeChild(spans[i]);
+    }
+  }
+}
+
+function remove_html_tag(tag, target_div){
+  var content = $(tag).html();
+  var foundat = content.indexOf("<"+tag,foundat);
+  while (foundat>-1) {
+            f2=content.indexOf(">",foundat);
+    if (f2>-1) {
+      content=content.substr(0,foundat)+content.substr(f2+1,content.length);
+    }
+    
+    f2=content.indexOf("</"+tag+">",foundat);
+    
+    if (f2>-1) {
+      content=content.substr(0,f2)+content.substr(f2+3+tag.length,content.length);
+    }
+    foundat=content.indexOf("<"+tag,foundat);
+  }
+  $(target_div).html(content);
 }
