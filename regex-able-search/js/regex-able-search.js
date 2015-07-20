7c9546b59f196716d06f1fc0607443ac953cdc44
Kimbsy <dave.kimber@simitive.com>
2015-07-20 15:39:56 +0100

    #regex-able-search replaces matches with match, not regex

--- regex-able-search/js/regex-able-search.js
+++ regex-able-search/js/regex-able-search.js
@@ -14,18 +14,21 @@ $(document).ready(function() {
 
   // listen for keypresses
   $(document).keydown(function(e) {
+
+    console.log('wooooo');
+
     // on ctrl + shift + f
     if (e.ctrlKey && e.shiftKey && e.keyCode == 70) {
       if (showing) {
-        searchContainer.slideUp();
+        searchContainer.slideUp('fast');
         showing = false;
 
-        // remove_html_tag('span', 'html');
-        // $('html').regexReplace('<\/?span[^>]*>');
+        // remove all current match spans
         $('span.regex-match').contents().unwrap();
+        $('span.regex-match').remove();
       }
       else {
-        searchContainer.slideDown();
+        searchContainer.slideDown('fast');
         showing = true;
 
         // select text
@@ -33,89 +36,53 @@ $(document).ready(function() {
       }
     }
 
+    // close search
+    if (showing) {
+      if (e.keyCode == 27) {
+        searchContainer.slideUp('fast');
+        showing = false;
+
+        // remove all current match spans
+        $('span.regex-match').contents().unwrap();
+        $('span.regex-match').remove();
+      }
+    }
+
     // submit search string
     if (showing) {
       if (e.keyCode == 13) {
-        $('html').regexReplace(searchInput.val());
+        // remove all current match spans
+        $('span.regex-match').contents().unwrap();
+        $('span.regex-match').remove();
+
+        // wrap matches in highlight spans
+        $('html').regexSpanWrap(searchInput.val());
+
+        // reselect text
+        searchInput.select();
       }
     }
   });
 
 });
 
-$.fn.regexReplace = function(regexString) {
+$.fn.regexSpanWrap = function(regexString) {
 
+  // create regex from string
   var pattern = new RegExp(regexString, 'g');
-  var repl = '<span class="regex-match">' + regexString + '</span>';
    
+  // loop over all nodes
   this.each(function() {
     $(this).contents().each(function() {
       if(this.nodeType === 3 && pattern.test(this.nodeValue)) {
-        $(this).replaceWith(this.nodeValue.replace(pattern, repl));
+        $(this).replaceWith(this.nodeValue.replace(pattern, function(match) {
+          return '<span class="regex-match">' + match + '</span>';
+        }));
       }
       else if(!$(this).hasClass('high')) {
-        $(this).regexReplace(regexString);
+        $(this).regexSpanWrap(regexString);
       }
     });
   });
   return this;
-
-  // // create regex string
-  // var regex = new RegExp('(' + regexString + ')', 'g');
-
-  // // console.log('wooooo');
-
-  // // // get text from page
-  // // // var pageHTML = $('html').html();
-
-  // // get all nodes
-  // var nodes = $('*');
-
-  // nodes.each(function() {
-  //   // console.log($(this).text());
-  //   if ($(this).text()) {
-  //     console.log($(this).text());
-  //     var oldText = $(this).text() || '';
-  //     $(this).text(oldText.replace(regex, '<span class="regex-match">$1</span>'));
-  //   }
-  // });
 };
-
-function removeSpans() {
-  var spans = document.getElementsByTagName("span");
-
-  for(var i=0; i<spans.length;i++)
-  {
-    if(spans[i].className == "regex-match")
-    {
-      var container = spans[i].parentNode;
-      var text = spans[i].innerHTML;
-      container.innerHTML += text;
-      container.removeChild(spans[i]);
-    }
-    else if(spans[i].className == "removed")
-    {
-      var container = spans[i].parentNode;
-      container.removeChild(spans[i]);
-    }
-  }
-}
-
-function remove_html_tag(tag, target_div){
-  var content = $(tag).html();
-  var foundat = content.indexOf("<"+tag,foundat);
-  while (foundat>-1) {
-            f2=content.indexOf(">",foundat);
-    if (f2>-1) {
-      content=content.substr(0,foundat)+content.substr(f2+1,content.length);
-    }
-    
-    f2=content.indexOf("</"+tag+">",foundat);
-    
-    if (f2>-1) {
-      content=content.substr(0,f2)+content.substr(f2+3+tag.length,content.length);
-    }
-    foundat=content.indexOf("<"+tag,foundat);
-  }
-  $(target_div).html(content);
-}
