106fc783464f06e0a13d2878852b506de1fe7634
Kimbsy <dave.kimber@simitive.com>
2015-07-20 16:43:31 +0100

    #regex-able-search now highlights and scrolls to current match, enter loops through matches, shift+enter loops back

--- regex-able-search/js/regex-able-search.js
+++ regex-able-search/js/regex-able-search.js
@@ -11,11 +11,14 @@ $(document).ready(function() {
   searchContainer.hide();
   var showing = false;
 
+  // variable for which match we are looking at
+  var matchIndex = 0;
+
+  // variable to store the current search string
+  var currentInput = '';
 
   // listen for keypresses
   $(document).keydown(function(e) {
-
-    console.log('wooooo');
 
     // on ctrl + shift + f
     if (e.ctrlKey && e.shiftKey && e.keyCode == 70) {
@@ -26,6 +29,12 @@ $(document).ready(function() {
         // remove all current match spans
         $('span.regex-match').contents().unwrap();
         $('span.regex-match').remove();
+
+        // reset match index
+        matchIndex = 0;
+
+        // reset current input
+        currentInput = '';
       }
       else {
         searchContainer.slideDown('fast');
@@ -45,41 +54,85 @@ $(document).ready(function() {
         // remove all current match spans
         $('span.regex-match').contents().unwrap();
         $('span.regex-match').remove();
+
+        // reset match index
+        matchIndex = 0;
+
+        // reset current input
+        currentInput = '';
       }
     }
 
     // submit search string
     if (showing) {
       if (e.keyCode == 13) {
-        // remove all current match spans
-        $('span.regex-match').contents().unwrap();
-        $('span.regex-match').remove();
+        if (searchInput.val() == currentInput) {
+          // look at the next/previous match
+          if (e.shiftKey) {
+            matchIndex--;
+          }
+          else {
+            matchIndex++;
+          }
+        }
+        else {
+          // remove all current match spans
+          $('span.regex-match').contents().unwrap();
+          $('span.regex-match').remove();
 
-        // wrap matches in highlight spans
-        $('html').regexSpanWrap(searchInput.val());
+          // wrap matches in highlight spans
+          $('html').regexSpanWrap(searchInput.val());
 
-        // reselect text
-        searchInput.select();
+          // set the current search string
+          currentInput = searchInput.val();
+
+          // reselect text
+          searchInput.select();          
+        }
+        // find out how many matches there are
+        var matches = $('.regex-match');
+        
+        // make sure we loop through matches
+        if (matchIndex > matches.length - 1) {
+          matchIndex = 0;
+        }
+        if (matchIndex < 0) {
+          matchIndex = matches.length - 1;
+        }
+        
+        // remove all current classes
+        matches.each(function() {
+          $(this).removeClass('regex-match-current');
+        });
+
+        // add current class to the current one
+        $(matches[matchIndex]).addClass('regex-match-current');
+        
+        // scroll to it
+        matches[matchIndex].scrollIntoView();
       }
     }
   });
-
 });
 
 $.fn.regexSpanWrap = function(regexString) {
 
   // create regex from string
   var pattern = new RegExp(regexString, 'g');
-   
+
   // loop over all nodes
   this.each(function() {
+    // loop over all the contents of this node
     $(this).contents().each(function() {
+      // if it's a text node
       if(this.nodeType === 3 && pattern.test(this.nodeValue)) {
+        // replace the match with the match wrapped in a highlighting span
         $(this).replaceWith(this.nodeValue.replace(pattern, function(match) {
           return '<span class="regex-match">' + match + '</span>';
         }));
       }
-      else if(!$(this).hasClass('high')) {
+      // recurse
+      else if(!$(this).hasClass('regex-match')) {
         $(this).regexSpanWrap(regexString);
       }
     });
