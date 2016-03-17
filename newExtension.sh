9104adf399ff51f1acef4c0626659d99ccfac9e3
Kimbsy <dave.kimber@simitive.com>
2016-03-17 10:22:45 +0000

    sjs red header

--- newExtension.sh
+++ newExtension.sh
@@ -36,7 +36,7 @@ new-ext() {
 
   # put content in files
   # manifest file
-  printf "{\n\t\"manifest_version\": 2,\n\t\"name\": \"$NAME\",\n\t\"description\": \"$DESC\",\n\n\t\"version\": \"1.0\",\n\n\t// You can add icon images if you want\n\t// \"icons\":\n\t// {\n\t// \t\"48\": \"$NAME-icon48.png\",\n\t// \t\"128\": \"$NAME-icon128.png\"\n\t// },\n\n\t\"content_scripts\": [\n\t\t{\n\t\t\"matches\": [\"<all_urls>\"],\n\t\t\"js\": [\"js/jquery-2.1.1.js\", \"js/$NAME.js\"],\n\t\t\"css\": [\"css/$NAME.css\"]\n\t\t}\n\t]\n}\n" > $NAME/manifest.json
+  printf "{\n    \"manifest_version\": 2,\n    \"name\": \"$NAME\",\n    \"description\": \"$DESC\",\n\n    \"version\": \"1.0\",\n\n    // You can add icon images if you want\n    // \"icons\":\n    // {\n    //     \"48\": \"$NAME-icon48.png\",\n    //     \"128\": \"$NAME-icon128.png\"\n    // },\n\n    \"content_scripts\": [\n        {\n        \"matches\": [\"<all_urls>\"],\n        \"js\": [\"js/jquery-2.1.1.js\", \"js/$NAME.js\"],\n        \"css\": [\"css/$NAME.css\"]\n        }\n    ]\n}\n" > $NAME/manifest.json
   # js skeleton
   printf "/**\n * JavaScript file to be loaded into matched pages\n **/\n\$(document).ready(function() {\n\n});\n" > $NAME/js/$NAME.js
   # css file
